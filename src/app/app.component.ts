74f336646f9d142e0db09ac884030950b5717626
ruchira2104 <naikruchira21@gmail.com>
2024-06-17 12:59:51 +0530

    angular view child and view children decorative

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, QueryList, ViewChild, ViewChildren, viewChild } from '@angular/core';
 import {COURSES} from '../db-data';
+import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+  
   courses = COURSES
+
+  // we can also refer to the template using template ref
+
+  //if we want to query deeper into the component hierarchy we can use viewChildren decorative
+  // @ViewChild('cardRef')
+  @ViewChildren(CourseCardComponent)
+  // @ViewChild(CourseCardComponent)
+  card : QueryList<CourseCardComponent>;
+
+  ngAfterViewInit() {
+    console.log(this.card.last);
+  }
+  onCourseSelected(course:Course) {
+    // console.log("called")
+    // console.log(course);
+    
+  }
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,38 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Course } from '../model/course';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Course } from "../model/course";
 
 @Component({
-  selector: 'course-card',
+  selector: "course-card",
   // standalone: true,
   // imports: [],
-  templateUrl: './course-card.component.html',
-  styleUrl: './course-card.component.css'
+  templateUrl: "./course-card.component.html",
+  styleUrl: "./course-card.component.css",
 })
-export class CourseCardComponent{
+export class CourseCardComponent {
   @Input({
-    required:true
+    required: true,
   })
-  course:Course;
+  course: Course;
 
-  @Input({
-    required:true
-  })
-  count:number;
+  // @Input({
+  //   required:true
+  // })
+  // count:number;
 
-  @Input({
-    required:true
-  })
-  index:number
- 
-  onCourseViewed(){
-    console.log("course clicked n viewd")
+  // @Input({
+  //   required:true
+  // })
+  // index:number
+
+  @Output("courseSelected")
+  courseEmitter = new EventEmitter<Course>();
+
+  onCourseViewed() {
+    this.courseEmitter.emit(this.course);
   }
 
-  getCardClass(){
+  getCardClass() {
     return {
-      'beginner': this.course.category === 'BEGINNER',
-      'otherClass':false
-    }
+      beginner: this.course.category === "BEGINNER",
+      otherClass: false,
+    };
 
     // ng Class can have configuration object like this or array of strings or single string also
   }
